54acf7c190b7ce6daea6797e457a17d8937d888a
Juris Gekiss <gekiss@inbox.lv>
2023-05-10 19:07:51 +0300

    Added RequestSecurityToken LifeTime element serialization

--- src/Microsoft.IdentityModel.Protocols.WsTrust/WsTrustSerializer.cs
+++ src/Microsoft.IdentityModel.Protocols.WsTrust/WsTrustSerializer.cs
@@ -422,6 +422,10 @@ namespace Microsoft.IdentityModel.Protocols.WsTrust
                 {
                     trustRequest.RequestType = WsUtils.ReadStringElement(reader);
                 }
+                else if (reader.IsStartElement(WsTrustElements.Lifetime, serializationContext.TrustConstants.Namespace))
+                {
+                    trustRequest.Lifetime = ReadLifetime(reader, serializationContext);
+                }
                 else if (reader.IsStartElement(WsTrustElements.OnBehalfOf, serializationContext.TrustConstants.Namespace))
                 {
                     trustRequest.OnBehalfOf = ReadOnBehalfOf(reader, serializationContext);
@@ -1284,6 +1288,9 @@ namespace Microsoft.IdentityModel.Protocols.WsTrust
                     attribute.WriteTo(writer);
 
                 writer.WriteElementString(serializationContext.TrustConstants.Prefix, WsTrustElements.RequestType, serializationContext.TrustConstants.Namespace, trustRequest.RequestType);
+
+                if (trustRequest.Lifetime != null)
+                    WriteLifetime(writer, serializationContext, trustRequest.Lifetime);
 
                 if (!string.IsNullOrEmpty(trustRequest.TokenType))
                     writer.WriteElementString(serializationContext.TrustConstants.Prefix, WsTrustElements.TokenType, serializationContext.TrustConstants.Namespace, trustRequest.TokenType);
